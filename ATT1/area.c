c0fccecc6c1c26458786c9ebeaf1d0e0628e5973
Matheus Gnann <119635234+mmagc@users.noreply.github.com>
2025-03-06 10:25:17 -0300

    Update area.c

--- ATT1/area.c
+++ ATT1/area.c
@@ -8,10 +8,8 @@ int main() {
     printf("Digite o raio do circulo: ");
     scanf("%f", &raio);
 
-    // Calcula a área do círculo (A = π * r²)
     area = PI * (raio * raio);
 
-    // Calcula o perímetro do círculo (P = 2 * π * r)
     perimetro = 2 * PI * raio;
 
     printf("Area do circulo: %.2f\n", area);
